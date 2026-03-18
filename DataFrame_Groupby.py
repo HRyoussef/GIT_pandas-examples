854b952552c7057bfd5bb56bf943bee03b1c8606
elora <elora.bellemare@le-campus-numerique.fr>
2026-03-18 10:20:41 +0100

    Changement elora

--- DataFrame_Groupby.py
+++ DataFrame_Groupby.py
@@ -60,7 +60,8 @@ print('\n==================================================================\n')
 
 
 # Sort data ratings by created field (groupby + lambda function + sorted)
-sortRatingsField = cloneDF(mergeRatings)
-sortRatingsField = sortRatingsField.groupby(['movie_id', 'title'])['rating'].agg(
-    COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort('COUNT', ascending=False)
-print('My info sorted: \n%s' % sortRatingsField[:15])
+sRF= cloneDF(mergeRatings)
+
+sRF= sRF.groupby(['movie_id', 'title'])['rating'].agg(
+    COUNT=np.size, myAVG=lambda x: x.sum() / float(x.count())).sort.values('COUNT', ascending=False)
+print('My info sorted: \n%s' % sRF[:15])
